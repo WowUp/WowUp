bc1f0ccc7b6ea882d51b2b03175cf35f42eb0d42
john liddell <jliddev@gmail.com>
2020-06-28 23:45:12 -0500

    add Mac

---
+++ WowUp.Mac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace WowUp.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
