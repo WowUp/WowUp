f19643cce8d7350cfdc863a6a57717c40ea80f41
ayell <duflot.axel@gmail.com>
2020-10-24 00:52:24 +0200

    [fix] async enable button

--- wowup-electron/src/app/components/wow-client-options/wow-client-options.component.ts
+++ wowup-electron/src/app/components/wow-client-options/wow-client-options.component.ts
@@ -1,39 +1,47 @@
-import { ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSelectChange } from '@angular/material/select';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import * as AdmZip from "adm-zip";
 import { WowClientType } from 'app/models/warcraft/wow-client-type';
 import { AddonChannelType } from 'app/models/wowup/addon-channel-type';
 import { ElectronService } from 'app/services';
 import { WarcraftService } from 'app/services/warcraft/warcraft.service';
 import { WowUpService } from 'app/services/wowup/wowup.service';
 import { getEnumList, getEnumName } from 'app/utils/enum.utils';
-import * as _ from 'lodash';
-import * as path from 'path';
 import * as fs from 'fs';
+import * as _ from 'lodash';
+import { isEmpty } from 'lodash';
+import * as path from 'path';
 import { Subscription } from 'rxjs';
 import { AlertDialogComponent } from '../alert-dialog/alert-dialog.component';
-import * as AdmZip from 'adm-zip';
-import { MatSnackBar } from '@angular/material/snack-bar';
-
-@Component({
+import { RestoreDialogComponent } from '../restore-dialog/restore-dialog.component';
+@Component( {
   selector: 'app-wow-client-options',
   templateUrl: './wow-client-options.component.html',
   styleUrls: ['./wow-client-options.component.scss']
-})
+} )
 export class WowClientOptionsComponent implements OnInit, OnDestroy {
 
-  @Input('clientType') clientType: WowClientType;
-
+  @Input( 'clientType' ) clientType: WowClientType;
+  @ViewChild( 'myButton' ) button;
   private subscriptions: Subscription[] = [];
 
   public clientTypeName: string;
   public clientFolderName: string;
   public clientLocation: string;
   public selectedAddonChannelType: AddonChannelType;
-  public addonChannelInfos: { type: AddonChannelType, name: string }[] = getEnumList(AddonChannelType)
-    .map((type: AddonChannelType) => ({ type: type, name: getEnumName(AddonChannelType, type) }));
+  public addonChannelInfos: { type: AddonChannelType, name: string }[] = getEnumList( AddonChannelType )
+    .map( ( type: AddonChannelType ) => ( { type: type, name: getEnumName( AddonChannelType, type ) } ) );
   public clientAutoUpdate: boolean;
+
+
+  public disabledBackup: boolean;
+  public disabledRestore: boolean;
+
+  private _fullPath: string;
+  private _pathBackup: string;
 
   constructor(
     private _dialog: MatDialog,
@@ -41,94 +49,118 @@ export class WowClientOptionsComponent implements OnInit, OnDestroy {
     private _warcraftService: WarcraftService,
     private _wowupService: WowUpService,
     private _cdRef: ChangeDetectorRef,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private _cd: ChangeDetectorRef
   ) {
-    const warcraftProductSubscription = this._warcraftService.products$.subscribe(products => {
-      const product = products.find(p => p.clientType === this.clientType);
-      if (product) {
+    const warcraftProductSubscription = this._warcraftService.products$.subscribe( products => {
+      const product = products.find( p => p.clientType === this.clientType );
+      if ( product ) {
         this.clientLocation = product.location;
         this._cdRef.detectChanges();
       }
-    });
+    } );
 
-    this.subscriptions.push(warcraftProductSubscription);
+    this.subscriptions.push( warcraftProductSubscription );
   }
 
   ngOnInit(): void {
-    this.selectedAddonChannelType = this._wowupService.getDefaultAddonChannel(this.clientType);
-    this.clientAutoUpdate = this._wowupService.getDefaultAutoUpdate(this.clientType);
-    this.clientTypeName = getEnumName(WowClientType, this.clientType);
-    this.clientFolderName = this._warcraftService.getClientFolderName(this.clientType);
-    this.clientLocation = this._warcraftService.getClientLocation(this.clientType);
+    this.selectedAddonChannelType = this._wowupService.getDefaultAddonChannel( this.clientType );
+    this.clientAutoUpdate = this._wowupService.getDefaultAutoUpdate( this.clientType );
+    this.clientTypeName = getEnumName( WowClientType, this.clientType );
+    this.clientFolderName = this._warcraftService.getClientFolderName( this.clientType );
+    this.clientLocation = this._warcraftService.getClientLocation( this.clientType );
+    this.disabledBackup = true;
+    this.disabledRestore = true;
+
+    isEmpty( this.clientLocation );
+
+    if ( !isEmpty( this.clientLocation ) ) {
+      this._fullPath = `${this.clientLocation}/${this.clientFolderName}`;
+      this._pathBackup = `${this._fullPath}/WowUp_backup`;
+      this.disabledBackup = false;
+      this.disabledRestore = !fs.existsSync( this._pathBackup )
+    }
   }
 
   ngOnDestroy(): void {
-    this.subscriptions.forEach((sub) => sub.unsubscribe());
+    this.subscriptions.forEach( ( sub ) => sub.unsubscribe() );
   }
 
-  onDefaultAddonChannelChange(evt: MatSelectChange) {
-    this._wowupService.setDefaultAddonChannel(this.clientType, evt.value);
+  onDefaultAddonChannelChange( evt: MatSelectChange ) {
+    this._wowupService.setDefaultAddonChannel( this.clientType, evt.value );
   }
 
-  onDefaultAutoUpdateChange(evt: MatSlideToggleChange) {
-    this._wowupService.setDefaultAutoUpdate(this.clientType, evt.checked);
+  onDefaultAutoUpdateChange( evt: MatSlideToggleChange ) {
+    this._wowupService.setDefaultAutoUpdate( this.clientType, evt.checked );
   }
 
   async onSelectClientPath() {
-    const selectedPath = await this.selectWowClientPath(this.clientType);
-    if (selectedPath) {
+    const selectedPath = await this.selectWowClientPath( this.clientType );
+    if ( selectedPath ) {
       this.clientLocation = selectedPath;
+      this.disabledBackup = isEmpty( this.clientLocation );
     }
   }
 
-  onCreateBackup() {
-
-    const path = `${this.clientLocation}/${this.clientFolderName}`;
-    const pathBackup = `${path}/backup`;
-
-    if (!fs.existsSync(pathBackup)) {
-      fs.mkdirSync(pathBackup)
+  async onCreateBackup() {
+    if ( !fs.existsSync( this._pathBackup ) ) {
+      fs.mkdirSync( this._pathBackup )
     };
 
-    this._snackBar.open("PAGES.OPTIONS.WOW.BACKUP_STARTED", "", { duration: 2000 });
-    setTimeout(() => {
+
+    this._snackBar.open( "PAGES.OPTIONS.WOW.BACKUP_STARTED" );
+
+
+    setTimeout( () => {
       const zip = new AdmZip();
-      zip.addLocalFolder(`${path}/WTF`, 'WTF');
-      zip.addLocalFolder(`${path}/Interface`, 'Interface');
-      zip.writeZip(`${pathBackup}/backup_${Date.now()}.zip`);
-    }, 1000);
+      zip.addLocalFolder( `${this._fullPath}/WTF`, 'WTF' );
+      zip.addLocalFolder( `${this._fullPath}/Interface`, 'Interface' );
+      zip.writeZip( `${this._pathBackup}/backup_${Date.now()}.zip` );
+      this.disabledRestore = false;
+      this._snackBar.dismiss();
+      this._cd.detectChanges()
+    }, 1000 );
+
+
   }
 
-  private async selectWowClientPath(clientType: WowClientType): Promise<string> {
-    const dialogResult = await this._electronService.remote.dialog.showOpenDialog({
+  onGetBackups() {
+    if ( !fs.existsSync( this._pathBackup ) ) {
+      return
+    };
+    this._dialog.open( RestoreDialogComponent )
+  }
+
+  private async selectWowClientPath( clientType: WowClientType ): Promise<string> {
+    const dialogResult = await this._electronService.remote.dialog.showOpenDialog( {
       properties: ['openDirectory']
-    });
+    } );
 
-    if (dialogResult.canceled) {
+    if ( dialogResult.canceled ) {
       return '';
     }
 
-    const selectedPath = _.first(dialogResult.filePaths);
-    if (!selectedPath) {
-      console.warn('No path selected')
+    const selectedPath = _.first( dialogResult.filePaths );
+    if ( !selectedPath ) {
+      console.warn( 'No path selected' )
       return '';
     }
 
-    console.log('dialogResult', selectedPath);
+    console.log( 'dialogResult', selectedPath );
 
-    if (this._warcraftService.setWowFolderPath(clientType, selectedPath)) {
+    if ( this._warcraftService.setWowFolderPath( clientType, selectedPath ) ) {
       return selectedPath;
     }
 
-    const clientFolderName = this._warcraftService.getClientFolderName(clientType);
-    const clientExecutableName = this._warcraftService.getExecutableName(clientType);
-    const clientExecutablePath = path.join(selectedPath, clientFolderName, clientExecutableName);
-    const dialogRef = this._dialog.open(AlertDialogComponent, {
+    const clientFolderName = this._warcraftService.getClientFolderName( clientType );
+    const clientExecutableName = this._warcraftService.getExecutableName( clientType );
+    const clientExecutablePath = path.join( selectedPath, clientFolderName, clientExecutableName );
+    const dialogRef = this._dialog.open( AlertDialogComponent, {
       data: {
         title: `Alert`,
-        message: `Unable to set "${selectedPath}" as your ${getEnumName(WowClientType, clientType)} folder.\nPath not found: "${clientExecutablePath}".`
+        message: `Unable to set "${selectedPath}" as your ${getEnumName( WowClientType, clientType )} folder.\nPath not found: "${clientExecutablePath}".`
       }
-    });
+    } );
 
     await dialogRef.afterClosed().toPromise();
 
