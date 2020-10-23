dadf0695c38e476b63a80785d6034991e8d1f80f
Dean Campbell <thedeancampbell@gmail.com>
2020-10-22 23:33:06 -0700

    Fix get addons version/releasedAt not showing
    If the addon does not have a release for the current default addon
    channel, fallback on the most recent release.

--- wowup-electron/src/app/pipes/get-addon-list-item-file-prop.pipe.ts
+++ wowup-electron/src/app/pipes/get-addon-list-item-file-prop.pipe.ts
@@ -1,13 +1,17 @@
 import { Pipe, PipeTransform } from "@angular/core";
 import { GetAddonListItem } from "../business-objects/get-addon-list-item";
 import { AddonChannelType } from "../models/wowup/addon-channel-type";
+import * as _ from 'lodash';
 
 @Pipe({
 	name: "getAddonListItemFileProp",
 })
 export class GetAddonListItemFilePropPipe implements PipeTransform {
 	transform(item: GetAddonListItem, prop: string, channel: AddonChannelType): any {
-		const file = item.getLatestFile(channel);
+		let file = item.getLatestFile(channel);
+		if (!file) {
+			file = _.first(_.orderBy(item.searchResult.files, 'releaseDate', 'desc'));
+		}
 		return file && file.hasOwnProperty(prop) ? file[prop] : '';
 	}
 }
