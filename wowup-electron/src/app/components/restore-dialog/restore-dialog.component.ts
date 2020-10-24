fe823e441ead3412dcf127f6fe2efb4b794d7538
ayell <duflot.axel@gmail.com>
2020-10-24 19:41:09 +0200

    [fix] remove folders corrupted

--- wowup-electron/src/app/components/restore-dialog/restore-dialog.component.ts
+++ wowup-electron/src/app/components/restore-dialog/restore-dialog.component.ts
@@ -6,61 +6,63 @@ import * as AdmZip from "adm-zip";
 import * as fs from 'fs';
 import * as moment from 'moment';
 import * as rimraf from 'rimraf';
-@Component({
+@Component( {
   selector: 'app-restore-dialog',
   templateUrl: './restore-dialog.component.html',
   styleUrls: ['./restore-dialog.component.scss']
-})
+} )
 export class RestoreDialogComponent implements OnInit {
   displayedColumns: string[] = ['Date', 'Restore', 'Delete'];
   dataSource: string[];
 
 
   private _archives: string[];
-  constructor(public dialogRef: MatDialogRef<RestoreDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { pathBackup: string, fullPath: string },
+  constructor( public dialogRef: MatDialogRef<RestoreDialogComponent>,
+    @Inject( MAT_DIALOG_DATA ) public data: { pathBackup: string, fullPath: string },
     private _snackBar: MatSnackBar,
-    private _translate: TranslateService,) {
+    private _translate: TranslateService, ) {
 
-    this._archives = fs.readdirSync(this.data.pathBackup);
+    this._archives = fs.readdirSync( this.data.pathBackup );
   }
 
   ngOnInit(): void {
     this.dataSource = this._setDataSource();
   }
 
-  deleteArchive(index: number) {
-    fs.unlinkSync(`${this.data.pathBackup}/${this._archives[index]}`);
-    this._archives.splice(index, 1);
+  deleteArchive( index: number ) {
+    fs.unlinkSync( `${this.data.pathBackup}/${this._archives[index]}` );
+    this._archives.splice( index, 1 );
     this.dataSource = this._setDataSource();
-    this._snackBar.open(this._translate.instant("PAGES.OPTIONS.RESTORE.BACKUP_REMOVED"), "", { duration: 2000 });
+    this._snackBar.open( this._translate.instant( "PAGES.OPTIONS.RESTORE.BACKUP_REMOVED" ), "", { duration: 2000 } );
   }
 
-  restoreBackup(index: number) {
-    rimraf.sync(`${this.data.fullPath}/WTF.old`);
-    rimraf.sync(`${this.data.fullPath}/Interface.old`);
+  restoreBackup( index: number ) {
+    rimraf.sync( `${this.data.fullPath}/WTF.old` );
+    rimraf.sync( `${this.data.fullPath}/Interface.old` );
     try {
-      fs.renameSync(`${this.data.fullPath}/WTF`, `${this.data.fullPath}/WTF.old`);
-      fs.renameSync(`${this.data.fullPath}/Interface`, `${this.data.fullPath}/Interface.old`);
-      this._snackBar.open(this._translate.instant("PAGES.OPTIONS.RESTORE.RESTORE_STARTED"));
+      fs.renameSync( `${this.data.fullPath}/WTF`, `${this.data.fullPath}/WTF.old` );
+      fs.renameSync( `${this.data.fullPath}/Interface`, `${this.data.fullPath}/Interface.old` );
+      this._snackBar.open( this._translate.instant( "PAGES.OPTIONS.RESTORE.RESTORE_STARTED" ) );
 
-      setTimeout(() => {
-        const zip = new AdmZip(`${this.data.pathBackup}/${this._archives[index]}`);
-        zip.extractAllTo(this.data.fullPath);
+      setTimeout( () => {
+        const zip = new AdmZip( `${this.data.pathBackup}/${this._archives[index]}` );
+        zip.extractAllTo( this.data.fullPath );
         this._snackBar.dismiss();
 
-        rimraf.sync(`${this.data.fullPath}/WTF.old`);
-        rimraf.sync(`${this.data.fullPath}/Interface.old`);
-      }, 1000);
+        rimraf.sync( `${this.data.fullPath}/WTF.old` );
+        rimraf.sync( `${this.data.fullPath}/Interface.old` );
+      }, 1000 );
 
     } catch {
-      fs.renameSync(`${this.data.fullPath}/WTF.old`, `${this.data.fullPath}/WTF`);
-      fs.renameSync(`${this.data.fullPath}/Interface.old`, `${this.data.fullPath}/Interface`);
+      rimraf.sync( `${this.data.fullPath}/WTF` );
+      rimraf.sync( `${this.data.fullPath}/Interface` );
+      fs.renameSync( `${this.data.fullPath}/WTF.old`, `${this.data.fullPath}/WTF` );
+      fs.renameSync( `${this.data.fullPath}/Interface.old`, `${this.data.fullPath}/Interface` );
     }
   }
 
   private _setDataSource() {
-    return this._archives.map(arch => parseInt(arch.split('.')[0])).sort((a, b) => b - a).map((date) => moment(new Date(date)).format('L LT'));
+    return this._archives.map( arch => parseInt( arch.split( '.' )[0] ) ).sort( ( a, b ) => b - a ).map( ( date ) => moment( new Date( date ) ).format( 'L LT' ) );
   }
 
 }
