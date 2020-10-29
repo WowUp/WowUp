792ebfa41ab8e5f9a70cd56eb95e7e871b42afe8
jliddev <jliddev@gmail.com>
2020-10-29 14:38:37 -0500

    leave the hard coded channel in

--- wowup-electron/app-updater.ts
+++ wowup-electron/app-updater.ts
@@ -16,6 +16,7 @@ export function initializeAppUpdater(win: BrowserWindow) {
   autoUpdater.logger = log;
   autoUpdater.autoDownload = false;
   autoUpdater.allowPrerelease = true;
+  autoUpdater.channel = "alpha";
 
   autoUpdater.on("update-available", () => {
     log.info(APP_UPDATE_AVAILABLE);
