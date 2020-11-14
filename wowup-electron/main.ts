b50807a0d6b0bd78504f3e59221b6ec82d628006
jliddev <jliddev@gmail.com>
2020-11-14 10:31:17 -0600

    Update main.ts

--- wowup-electron/main.ts
+++ wowup-electron/main.ts
@@ -226,7 +226,7 @@ try {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     // if (process.platform !== "darwin") {
-      app.quit();
+    app.quit();
     // }
   });
 
