04436bf7371a3dad471f74d3d402a26ae5208cbe
김상현_Soaa <soaa@un7qi3.co>
2020-11-16 15:25:58 +0900

    - [i18n][ko] add a long number format for 4-digits based number-counting countries. (mainly east-asian countries,CJK)

--- wowup-electron/src/app/pipes/download-count.pipe.ts
+++ wowup-electron/src/app/pipes/download-count.pipe.ts
@@ -8,22 +8,32 @@ export class DownloadCountPipe implements PipeTransform {
   constructor(private translateService: TranslateService) {}
 
   transform(value: number, ...args: unknown[]): string {
-    let suffix = "";
-    let downloadCount = value;
-    if (downloadCount >= 1000000000) {
-      suffix = "BILLION";
-      downloadCount /= 1000000000.0;
-    } else if (downloadCount >= 1000000) {
-      suffix = "MILLION";
-      downloadCount /= 1000000.0;
-    } else if (downloadCount >= 1000) {
-      suffix = "THOUSAND";
-      downloadCount /= 1000.0;
-    }
-    return suffix
-      ? this.translateService.instant("COMMON.DOWNLOAD_COUNT." + suffix, {
+    let unit:number = Number(this.translateService.instant("COMMON.DOWNLOAD_COUNT.DIGIT_ABBR_UNIT"));
+    if (!unit || isNaN(unit)) {
+      let suffix = "";
+      let downloadCount = value;
+      if (downloadCount >= 1000000000) {
+        suffix = "BILLION";
+        downloadCount /= 1000000000.0;
+      } else if (downloadCount >= 1000000) {
+        suffix = "MILLION";
+        downloadCount /= 1000000.0;
+      } else if (downloadCount >= 1000) {
+        suffix = "THOUSAND";
+        downloadCount /= 1000.0;
+      }
+      return suffix
+        ? this.translateService.instant("COMMON.DOWNLOAD_COUNT." + suffix, {
           count: downloadCount.toFixed(0),
         })
-      : downloadCount.toString();
+        : downloadCount.toString();
+    } else {
+      let index = Math.floor(Math.log10(value) / unit);
+      let downloadCount = index === 0 ? value : value / Math.pow(Math.pow(10, unit), index);
+
+      return this.translateService.instant(`COMMON.DOWNLOAD_COUNT.ABBR_SUFFIXES.${index}`, {
+        count: downloadCount.toFixed(0)
+      });
+    }
   }
 }
