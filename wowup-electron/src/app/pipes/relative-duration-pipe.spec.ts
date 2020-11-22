49a7f04bbaf389c6d232b6d00d151129b87455ea
Lynn <kjarli@gmail.com>
2020-11-22 22:58:40 +0100

    Fixed relative-duration-pipe.spec.ts

--- wowup-electron/src/app/pipes/relative-duration-pipe.spec.ts
+++ wowup-electron/src/app/pipes/relative-duration-pipe.spec.ts
@@ -1,13 +1,44 @@
-import { DatePipe } from "@angular/common";
-import { inject } from "@angular/core/testing";
-import { TranslateService } from "@ngx-translate/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { Component } from "@angular/core";
+import { TranslateCompiler, TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { httpLoaderFactory } from "../app.module";
+import { TranslateMessageFormatCompiler } from "ngx-translate-messageformat-compiler";
 import { RelativeDurationPipe } from "./relative-duration-pipe";
 
+@Component({
+  template: `<p>{{ date | relativeDuration }}</p>`,
+})
+class TestRelativeDurationComponent {
+  public date = new Date().toString();
+}
+
 describe("RelativeDurationPipe", () => {
-  it("create an instance", () => {
-    inject([DatePipe, TranslateService], (datePipe: DatePipe, translateService: TranslateService) => {
-      const pipe = new RelativeDurationPipe(translateService);
-      expect(pipe).toBeTruthy();
-    });
+  let component: TestRelativeDurationComponent;
+  let fixture: ComponentFixture<TestRelativeDurationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TestRelativeDurationComponent, RelativeDurationPipe],
+      imports: [HttpClientModule, TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient],
+        },
+        compiler: {
+          provide: TranslateCompiler,
+          useClass: TranslateMessageFormatCompiler,
+        },
+      })],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestRelativeDurationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
   });
 });
