0bf3c7a200eb1062177a3b7024e25b65a52ca5e7
Lynn <kjarli@gmail.com>
2020-11-22 21:00:38 +0100

    Fixed options.component.spec.ts

--- wowup-electron/src/app/pages/options/options.component.spec.ts
+++ wowup-electron/src/app/pages/options/options.component.spec.ts
@@ -1,25 +1,62 @@
-import { inject } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { ElectronService } from "../../services/electron/electron.service";
 import { WowUpService } from "../../services/wowup/wowup.service";
 import { OptionsComponent } from "./options.component";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { TranslateCompiler, TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { httpLoaderFactory } from "../../app.module";
+import { TranslateMessageFormatCompiler } from "ngx-translate-messageformat-compiler";
 
 describe("OptionsComponent", () => {
+  let component: OptionsComponent;
+  let fixture: ComponentFixture<OptionsComponent>;
+  let addonService: ElectronService;
+  let wowUpService: WowUpService;
+  let electronServiceSpy: any;
+  let wowUpServiceSpy: any;
+
+  beforeEach(async () => {
+    wowUpServiceSpy = jasmine.createSpyObj("WowUpService", {
+      getThemeLogoPath: () => "",
+    }, {
+      currentTheme: "horde ofc",
+    });
+    electronServiceSpy = jasmine.createSpyObj("ElectronService", ["a"], {
+      isWin : false,
+      isLinux : true,
+      isMax: false,
+    });
+
+    await TestBed.configureTestingModule({
+      declarations: [OptionsComponent],
+      imports: [HttpClientModule, TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient],
+        },
+        compiler: {
+          provide: TranslateCompiler,
+          useClass: TranslateMessageFormatCompiler,
+        },
+      })],
+    }).overrideComponent(OptionsComponent, {
+      set: {
+        providers: [
+          { provide: WowUpService, useValue: wowUpServiceSpy },
+          { provide: ElectronService, useValue: electronServiceSpy },
+        ]},
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(OptionsComponent);
+    component = fixture.componentInstance;
+    wowUpService = fixture.debugElement.injector.get(WowUpService);
+    addonService = fixture.debugElement.injector.get(ElectronService);
+
+    fixture.detectChanges();
+  });
+
   it("should create", () => {
-    inject(
-      [
-        WowUpService,
-        ElectronService,
-      ],
-      (
-        wowupService: WowUpService,
-        electronService: ElectronService,
-      ) => {
-        const instance = new OptionsComponent(
-          wowupService,
-          electronService,
-        );
-        expect(instance).toBeTruthy();
-      }
-    );
+    expect(component).toBeTruthy();
   });
 });
