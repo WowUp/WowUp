2bf657f537e73dc8c12bb3594a775414afa6ebe4
Lynn <kjarli@gmail.com>
2020-11-22 22:59:52 +0100

    Fixed progress-spinner.component.spec.ts

--- wowup-electron/src/app/components/progress-spinner/progress-spinner.component.spec.ts
+++ wowup-electron/src/app/components/progress-spinner/progress-spinner.component.spec.ts
@@ -1,5 +1,9 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { ProgressSpinnerComponent } from "./progress-spinner.component";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { TranslateCompiler, TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { httpLoaderFactory } from "../../app.module";
+import { TranslateMessageFormatCompiler } from "ngx-translate-messageformat-compiler";
 
 describe("ProgressSpinnerComponent", () => {
   let component: ProgressSpinnerComponent;
@@ -8,10 +12,19 @@ describe("ProgressSpinnerComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ProgressSpinnerComponent],
+      imports: [HttpClientModule, TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient],
+        },
+        compiler: {
+          provide: TranslateCompiler,
+          useClass: TranslateMessageFormatCompiler,
+        },
+      })],
     }).compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(ProgressSpinnerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
