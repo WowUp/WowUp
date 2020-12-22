fee7d8a548f4aaae8c2012b2ab22765d9b87316a
jliddev <jliddev@gmail.com>
2020-12-22 10:45:14 -0600

    rework the funding links in details dialog

---
+++ wowup-electron/src/app/components/funding-button/funding-button.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, OnInit } from "@angular/core";
+import { AddonFundingLink } from "../../entities/addon";
+
+@Component({
+  selector: "app-funding-button",
+  templateUrl: "./funding-button.component.html",
+  styleUrls: ["./funding-button.component.scss"],
+})
+export class FundingButtonComponent implements OnInit {
+  @Input("funding") funding: AddonFundingLink;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  getTooltipKey() {
+    return `PAGES.MY_ADDONS.FUNDING_TOOLTIP.${this.funding.platform.toUpperCase()}`;
+  }
+
+  getFundingIcon() {
+    switch (this.funding.platform) {
+      case "LIBERAPAY":
+        return "assets/images/librepay_logo_small.png";
+      case "PATREON":
+        return "assets/images/patreon_logo_white.png";
+      case "GITHUB":
+        return "assets/images/github_logo_small.png";
+      case "CUSTOM":
+      default:
+        return "assets/images/custom_funding_logo_small.png";
+    }
+  }
+
+  getFundingName() {
+    switch (this.funding.platform) {
+      case "LIBERAPAY":
+        return "Liberapay";
+      case "PATREON":
+        return "Patreon";
+      case "GITHUB":
+        return "GitHub";
+      case "PAYPAL":
+        return "PayPal";
+      case "CUSTOM":
+      default:
+        return "Custom";
+    }
+  }
+}
