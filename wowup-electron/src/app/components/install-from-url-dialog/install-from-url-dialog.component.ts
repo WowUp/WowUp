08f407a17d25646fc2c5d4f8991094b533b9b176
jliddev <jliddev@gmail.com>
2020-12-27 21:55:58 -0600

    Add some more specific errors for the install by url process

--- wowup-electron/src/app/components/install-from-url-dialog/install-from-url-dialog.component.ts
+++ wowup-electron/src/app/components/install-from-url-dialog/install-from-url-dialog.component.ts
@@ -10,6 +10,7 @@ import { TranslateService } from "@ngx-translate/core";
 import { roundDownloadCount, shortenDownloadCount } from "../../utils/number.utils";
 import { DownloadCountPipe } from "../../pipes/download-count.pipe";
 import { NO_SEARCH_RESULTS_ERROR } from "../../../common/constants";
+import { AssetMissingError, ClassicAssetMissingError, NoReleaseFoundError } from "../../errors";
 
 @Component({
   selector: "app-install-from-url-dialog",
@@ -122,6 +123,12 @@ export class InstallFromUrlDialogComponent implements OnInit, OnDestroy {
         message = this._translateService.instant("DIALOGS.INSTALL_FROM_URL.ERROR.FAILED_TO_CONNECT");
       } else if (message === NO_SEARCH_RESULTS_ERROR) {
         message = this._translateService.instant("DIALOGS.INSTALL_FROM_URL.ERROR.NO_SEARCH_RESULTS");
+      } else if (err instanceof ClassicAssetMissingError) {
+        message = this._translateService.instant("DIALOGS.INSTALL_FROM_URL.ERROR.CLASSIC_ASSET_NOT_FOUND", { message: err.message});
+      } else if (err instanceof AssetMissingError) {
+        message = this._translateService.instant("DIALOGS.INSTALL_FROM_URL.ERROR.ASSET_NOT_FOUND", { message: err.message});
+      } else if( err instanceof NoReleaseFoundError){
+        message = this._translateService.instant("DIALOGS.INSTALL_FROM_URL.ERROR.NO_RELEASE_FOUND", { message: err.message});
       }
 
       this.showErrorMessage(message);
