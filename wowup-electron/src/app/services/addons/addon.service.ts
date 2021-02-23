a4393902d24c394108c4dde7202dd9e4724bfcfc
Lynn <kjarli@gmail.com>
2021-02-24 00:02:48 +0100

    Added from and to versions in logs during addon updates

--- wowup-electron/src/app/services/addons/addon.service.ts
+++ wowup-electron/src/app/services/addons/addon.service.ts
@@ -426,6 +426,8 @@ export class AddonService {
       throw new Error("Addon not found or invalid");
     }
 
+    console.log(`Started update for "${addon.name}" at version "${addon.installedVersion}" to "${addon.latestVersion}"`);
+
     const addonProvider = this.getProvider(addon.providerName);
     const downloadFileName = `${slug(addon.name)}.zip`;
 
@@ -532,6 +534,7 @@ export class AddonService {
         installState: AddonInstallState.Complete,
         progress: 100,
       });
+      console.log(`Finished update for "${addon.name}" at version "${addon.installedVersion}"`);
     } catch (err) {
       console.error(err);
       queueItem.completion.reject(err);
