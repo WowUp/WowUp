476958d71a069dd66ff0b2154dc476fe5cb03c35
jliddev <jliddev@gmail.com>
2021-03-23 12:05:20 -0500

    Fix tests

--- wowup-electron/src/app/components/animated-logo/animated-logo.component.ts
+++ wowup-electron/src/app/components/animated-logo/animated-logo.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from "@angular/core";
-import { ElectronService } from "app/services";
+import { Component } from "@angular/core";
+import { ElectronService } from "../../services";
 
 @Component({
   selector: "app-animated-logo",
