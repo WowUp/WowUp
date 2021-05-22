69c95bd4d8f420a4d55185e45b7070e431761b21
jliddev <jliddev@gmail.com>
2021-05-22 08:48:44 -0500

    Version bump

--- wowup-electron/src/app/services/wowup/patch-notes.service.ts
+++ wowup-electron/src/app/services/wowup/patch-notes.service.ts
@@ -23,6 +23,7 @@ const CHANGELOGS: ChangeLog[] = [
         <li>Installing from a GitHub URL now supports the new BigWigs metadata file</li>
         <li>Installing from a GitHub URL now supports -bcc zip files when a Burning Crusade client is selected</li>
         <li>The Get Addons tab will now also list Recently Added addons from CurseForge, just sort by the 'Released At' column</li>
+        <li>Using the WowUp Addon should no longer cause the Battle.net app to say "Updating" for all clients</li>
       </ul>
       </div>`,
   },
