d9fcec38f362f9389d2990a8b4daf3a37dc0a886
jliddev <jliddev@gmail.com>
2021-11-12 08:39:04 -0600

    Version bump

--- wowup-electron/src/app/services/wowup/patch-notes.service.ts
+++ wowup-electron/src/app/services/wowup/patch-notes.service.ts
@@ -16,11 +16,11 @@ export class PatchNotesService {
 
 const CHANGELOGS: ChangeLog[] = [
   {
-    Version: "2.5.2",
+    Version: "2.6.0",
     html: `
-    <h4 style="margin-top: 1em;">Hotfix</h4>
+    <h4 style="margin-top: 1em;">New Features</h4>
     <ul>
-    <li>Fix an issue with the guide url going to the wrong domain</li>
+    <li>Added the ability to switch to Beta build release channel from the app</li>
     </ul>`,
   },
   {
