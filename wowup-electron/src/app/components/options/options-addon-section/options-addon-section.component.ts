2abb0a60c3a3cf1cdb3a16f1a3d3be9387c4a69d
Lynn <kjarli@gmail.com>
2022-02-19 17:19:04 +0100

    Allow saving an empty key

--- wowup-electron/src/app/components/options/options-addon-section/options-addon-section.component.ts
+++ wowup-electron/src/app/components/options/options-addon-section/options-addon-section.component.ts
@@ -40,7 +40,7 @@ export class OptionsAddonSectionComponent implements OnInit, OnDestroy {
         takeUntil(this.destroy$),
         debounceTime(300),
         switchMap((ch) => {
-          if (ch.cfV2ApiKey) {
+          if ('cfV2ApiKey' in ch) {
             return from(this._sensitiveStorageService.setAsync(PREF_CF2_API_KEY, ch.cfV2ApiKey));
           }
           return of(undefined);
