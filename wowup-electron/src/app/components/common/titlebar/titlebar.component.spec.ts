c3587cdb21ae1bbeec61b7d7eb2e5fc3187cb69b
jliddev <jliddev@gmail.com>
2022-05-27 09:08:40 -0500

    Remove more curse stuff

--- wowup-electron/src/app/components/common/titlebar/titlebar.component.spec.ts
+++ wowup-electron/src/app/components/common/titlebar/titlebar.component.spec.ts
@@ -24,7 +24,7 @@ describe("TitlebarComponent", () => {
       "ElectronService",
       {
         on: undefined,
-        invoke: new Promise((res, rej) => {}),
+        invoke: new Promise(() => {}),
       },
       {
         windowMaximized$: new BehaviorSubject(false).asObservable(),
