49c1d84f4da5b94b765872b21b7190bc86e45072
jliddev <jliddev@gmail.com>
2022-12-27 15:01:45 -0600

    Version bump

--- wowup-electron/src/app/services/wowup/patch-notes.service.ts
+++ wowup-electron/src/app/services/wowup/patch-notes.service.ts
@@ -21,17 +21,17 @@ const CHANGELOGS: ChangeLog[] = [
     <h4 style="margin-top: 1em;">Changes</h4>
     <ul>
     <li>Dynamic WoW client names (CyanoHao)</li>
-    </ul>
-    <h4 style="margin-top: 1em;">Changes</h4>
-    <ul>
     <li>Spanish locale updates (SkollVargr)</li>
+    <li>Norwegian locale updates (espenja)</li>
     <li>German locale updates (maestrohdude)</li>
+    <li>Improved WowInterface API usage for some addons</li>
     <li>Improved private GitHub repo support</li>
     <li>Disable the ability to shift + click columns</li>
     <li>Update a bunch of dependencies</li>
     </ul>
     <h4 style="margin-top: 1em;">Fixes</h4>
     <ul>
+    <li>Fix an issue with My Addon columns appearing squished</li>
     <li>Rendering lists should no longer fail due to malformed game version</li>
     <li>Audio should now be muted just in case of an annoying ad</li>
     <li>Fix an issue with window not showing at startup on Mac</li>
