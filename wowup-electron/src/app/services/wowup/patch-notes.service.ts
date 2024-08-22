7b6a1764b783893c4bed9b231c502352c9be1be1
jliddev <jliddev@gmail.com>
2024-08-21 22:43:33 -0500

    Add patch notes and updated some client strings

--- wowup-electron/src/app/services/wowup/patch-notes.service.ts
+++ wowup-electron/src/app/services/wowup/patch-notes.service.ts
@@ -16,6 +16,15 @@ export class PatchNotesService {
 
 const CHANGELOGS: ChangeLog[] = [
   {
+    Version: "2.20.0",
+    html: `
+    <h4>Features</h4>
+    <ul>
+    <li>Add The War Within</li>
+    </ul>
+    `,
+  },
+  {
     Version: "2.12.0",
     html: `
     <h4>Features</h4>
